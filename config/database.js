9b51d8cdcc365c3ddb066ffc01b5fe59a86b8c91
Chino <67367208+ceeno007@users.noreply.github.com>
2024-08-10 12:38:18 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -103,19 +103,20 @@
 //   };
 // };
 
+
 module.exports = () => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: 'pg-35f88ea8-nnbba-dbf4.l.aivencloud.com',
-      port: 20539,
-      database: 'defaultdb',
-      user: 'avnadmin',
-      password: 'AVNS_sN9p8qHBNGHYlWacgXV',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: process.env.DATABASE_SSL_SELF === 'true',
       },
     },
-    debug: false,
+    debug: process.env.DATABASE_DEBUG === 'true',
   },
 });
