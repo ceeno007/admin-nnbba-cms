46d6ebd0a91c9b43192605ff46822784ca1ed6a1
chino <chinochinedu@gmail.com>
2024-08-10 01:20:47 +0100

    ghg

---
+++ config/bootstrap.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = async () => {
+    const url = `https://nnbba-admin.onrender.com/`; // Replace with your Render URL
+    const interval = 30000; // Interval in milliseconds (30 seconds)
+
+    function reloadWebsite() {
+        axios.get(url)
+            .then(response => {
+                console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+            })
+            .catch(error => {
+                console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+            });
+    }
+
+    setInterval(reloadWebsite, interval);
+};
